331944bafbb1da01b08497445f4b2def188b9b57
KalbeAbbas <kalbeabbas142@gmail.com>
2019-11-07 21:34:38 +0500

    Create uBit_example.py

---
+++ uBit_example.py
@@ -0,0 +1,28 @@
+from uBit_xCore import*
+
+from xSL06 import*
+
+
+
+core = xCore(0x39)  #SL06 I2C adddress
+SL06 = xSL06(core)  #Sending I2C object to SL06 lib
+
+SL06.enableLightSensor()
+
+while True:
+    
+    light = SL06.getAmbientLight()
+    red = SL06.getRedLight()
+    green = SL06.getGreenLight()
+    blue = SL06.getBlueLight()
+    print("Red")
+    print(red)
+    print("Blue")
+    print(blue)
+    print("Green")
+    print(green)
+    print("Light")
+    print(light)
+    
+    sleep(100)
+
