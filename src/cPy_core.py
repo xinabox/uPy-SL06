95e99982d9bc0e1fd60df23f629cc4df60bab421
KalbeAbbas <kalbeabbas142@gmail.com>
2019-11-08 15:52:02 +0500

    Create cPy_core.py

---
+++ src/cPy_core.py
@@ -0,0 +1,21 @@
+import board
+import busio
+ 
+i2c = busio.I2C(board.SCL, board.SDA)
+
+while not i2c.try_lock():
+    pass
+
+class xCore():
+    def __init__(self, i2c_addr):
+        self.addr = i2c_addr
+
+    def write_bytes(self, reg, val):
+        i2c.writeto(self.addr, bytearray([reg, val]))
+
+    def write_read(self, data, n):
+        buff = bytearray([0])
+        i2c.writeto(self.addr, bytearray([data]))
+        i2c.readfrom_into(self.addr, buff)
+        
+        return buff
