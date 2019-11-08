e00730c1c27086e7004a42c8f3a9767281a8e738
KalbeAbbas <kalbeabbas142@gmail.com>
2019-11-08 17:50:21 +0500

    Create cPy_SL06_proximity.py

---
+++ examples/cPy_SL06_proximity.py
@@ -0,0 +1,17 @@
+from cPy_core import*
+
+from xSL06_proximity import*
+
+core = xCore(0x39)  #SL06 I2C adddress
+SL06 = xSL06(core)  #Sending I2C object to SL06 lib
+
+SL06.enableProximitySensor()
+
+while True:
+    
+    prox = SL06.getProximity()
+    print("Proximity")
+    print(prox)
+    
+    sleep(500)
+
